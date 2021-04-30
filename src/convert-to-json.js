11a92fa184b4507e6b8adffb0937d666a80ec9ec
Fernando Lobos <fernando.lobos@latam.com>
2021-04-29 21:27:42 -0400

    Init

---
+++ src/convert-to-json.js
@@ -0,0 +1,13 @@
+const { FILE } = require('dns');
+const fs = require('fs');
+const recipes = require('./fake-data')
+
+const FILE_NAME = 'recipes.json';
+
+fs.writeFileSync(
+    FILE_NAME,
+    JSON.stringify(recipes),
+    'utf-8'
+)
+
+console.log('done')
